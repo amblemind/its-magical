13cd2a54870e2cbde8f8c7738d199398ac446514
Darren Alderman <darren@amblemind.com>
2022-11-22 05:13:26 +0000

    adding logging on function

--- pages/api/remix.js
+++ pages/api/remix.js
@@ -2,7 +2,11 @@ import AWS from 'aws-sdk'
 
 export default async function handler(req, res) {
 
+	console.log("start request")
+
 	const url = req.body.url;
+
+	console.log(url)
 
 	const S3 = new AWS.S3({
 		apiVersion: '2006-03-01',
@@ -11,6 +15,8 @@ export default async function handler(req, res) {
 	});
 
 	const S3_BUCKET = process.env.AWS_BUCKET;
+
+	console.log("bucket: " + S3_BUCKET)
 
 	// Perform URL validation
 	if (!url || !url.trim()) {
@@ -21,7 +27,7 @@ export default async function handler(req, res) {
 
 	try {
 
-
+		console.log("fecthing image")
 		// fetch from api with body and headers
 		const image = await fetch('https://chrome.browserless.io/screenshot?token=' + process.env.BROWSERLESS_TOKEN, {
 			method: 'POST',
@@ -34,14 +40,28 @@ export default async function handler(req, res) {
 			})
 		})	
 
+		console.log(image)
+
 		// upload to S3
 		const fileName = 'its_magical_' + Date.now() + '.png';
 
+		console.log("upload to s3")
 		await S3.upload({
 			Bucket: S3_BUCKET,
 			Key: fileName,
 			Body: image.body
 		}, (err, data) => {
+
+			if (err) {
+				console.log(err)
+				return res.status(500).json({
+					error: 'Something went wrong'
+				})	
+			}
+
+			console.log(data)
+
+			console.log("creating signed url")
 			var params = {Bucket: S3_BUCKET, Key: fileName};
 			var signedURL = S3.getSignedUrl('getObject', params);
 			console.log(signedURL);
